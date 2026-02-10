828fe8b429d6ba5a91b157cecc6902812dcece38
Diego Rojas Justiniano <diegorojasj222@gmail.com>
2026-02-10 19:26:45 -0400

    fix: create output directory if it doesn't exist

--- load.py
+++ load.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 
 def load_data(df: pd.DataFrame, target_file: str) -> None:
     """Load data from a BeautifulSoup object"""
+    os.makedirs(os.path.dirname(target_file), exist_ok=True)
     df.to_csv(target_file, index=False)
